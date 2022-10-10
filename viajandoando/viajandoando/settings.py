9dda2e7f8f06194c9d88f88bee92b0dc453d72dc
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2022-10-10 20:20:14 -0300

    correcciones perfiles, settings y demas

--- viajandoando/viajandoando/settings.py
+++ viajandoando/viajandoando/settings.py
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'viajandoando.ingweb@gmail.com'
 EMAIL_HOST_USER = 'viajandoando.ingweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'kxepoqtaakvqozhg'
+EMAIL_HOST_PASSWORD = os.getenv('DJANGO_EMAIL_HOST_PASSWORD','kxepoqtaakvqozhg')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 

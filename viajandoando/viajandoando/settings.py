3e79debd398a7aa3309a33603a93d7daeba8023b
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2022-10-10 20:30:00 -0300

    fix heroku mail settings

--- viajandoando/viajandoando/settings.py
+++ viajandoando/viajandoando/settings.py
@@ -136,9 +136,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'viajandoando.ingweb@gmail.com'
 EMAIL_HOST_USER = 'viajandoando.ingweb@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('DJANGO_EMAIL_HOST_PASSWORD','kxepoqtaakvqozhg')
+EMAIL_HOST_PASSWORD = os.getenv('DJANGO_EMAIL_HOST_PASSWORD','')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+'kxepoqtaakvqozhg'
 
 PASSWORD_RESET_TIMEOUT = 14400
 

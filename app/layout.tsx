862d1055bfc5af8ff8cabf1d5d10dc44b2308221
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-02-01 10:25:56 -0300

    reorganize and update imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar/navbar'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "../components/navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Viajando Ando',
-  description: 'Conectá con gente que quiera viajar con y como vos 🛣️',
-}
+    title: "Viajando Ando",
+    description: "Conectá con gente que quiera viajar con y como vos 🛣️",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="es">
-      <body className={inter.className}>
-        <Navbar></Navbar>
-        
-        <div className="container mx-auto bg-slate-100">
-          {children}
-        </div>
-        </body>
-    </html>
-  )
+    return (
+        <html lang="es">
+            <body className={inter.className}>
+                <Navbar></Navbar>
+
+                <div className="container mx-auto bg-slate-100">{children}</div>
+            </body>
+        </html>
+    );
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import viajandoAndoLogo from "/public/img/logo.png"
-import Image from 'next/image'
+import React from "react";
+import viajandoAndoLogo from "/public/img/logo.png";
+import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <div className='flex items-center justify-between px-6 py-3 bg-white shadow-lg'>
-        <Image src={viajandoAndoLogo} alt='Viajando Ando Logo' width={220}></Image>
-        <h2 className='text-lg'>Usuario</h2>
-    </div>
-  )
-}
+    return (
+        <div className="flex items-center justify-between px-6 py-3 bg-white shadow-lg">
+            <Image
+                src={viajandoAndoLogo}
+                alt="Viajando Ando Logo"
+                width={220}
+            ></Image>
+            <h2 className="text-lg">Usuario</h2>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
